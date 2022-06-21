66f3d4e4c432950e5fea568676115e53e9762a36
urbanDev <urbanoakoro1991@gmail.com>
2022-06-21 22:57:04 +0200

    He resuelto el problema del menu y estilizado algunas secciones

--- js/main.js
+++ js/main.js
@@ -3,20 +3,28 @@
 let menu = document.querySelector(".menu");
 let navMenu = document.querySelector(".nav-menu");
 let cerrar = document.querySelector(".cerrar-img");
+let link = document.querySelector(".link");
 
 
 menu.addEventListener("click", function() {
     document.querySelector(".nav-menu").style.display = "grid";
     document.querySelector(".menu").style.display = "none";
     document.querySelector(".cerrar-img").style.display = "block";
-    document.querySelector('body').style.overflow = 'hidden'; /* desactivamos el scroll cuando el menu no está activo */
+    document.querySelector(".body").style.overflow = 'hidden'; /* desactivamos el scroll cuando el menu no está activo */
 });
 
 cerrar.addEventListener("click", function() {
     document.querySelector(".nav-menu").style.display = "none";
-    document.querySelector(".menu").style.display = "none";
+    document.querySelector(".menu").style.display = "block";
     document.querySelector(".cerrar-img").style.display = "none";
     document.querySelector(".menu").style.display = "block";
-    document.querySelector('body').style.overflow = 'visible'; /* activamos el scroll cuando el menu no está activo */
-})
+    document.querySelector(".body").style.overflow = 'visible'; /* activamos el scroll cuando el menu no está activo */
+});
+
+    navMenu.addEventListener("click", function() {
+    document.querySelector(".body").style.overflow = 'visible';
+    document.querySelector(".nav-menu").style.display = "none";
+    document.querySelector(".cerrar-img").style.display = "none";
+    document.querySelector(".menu").style.display = "block";
+});
 
